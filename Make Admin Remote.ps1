dd95dc6d45dccec6547e46eff07b3beaae73317f
Hunt-j <59659712+Hunt-j@users.noreply.github.com>
2020-01-09 13:54:22 -0500

    Update Make Admin Remote.ps1

--- Make Admin Remote.ps1
+++ Make Admin Remote.ps1
@@ -4,11 +4,11 @@ $computername= Read-host "Computer Name"
     
 
 
-$s= New-PSSession -ComputerName $computername -Credential plymouth\adm-hjarvis
+$s= New-PSSession -ComputerName $computername -Credential "DOMAIN\USER"
 
 
 Invoke-Command -Session $s {$newadmin= Read-Host  "Username"}
-Invoke-Command -session $s {Add-LocalGroupMember -Group Administrators -Member "plymouth\$newadmin"}
+Invoke-Command -session $s {Add-LocalGroupMember -Group Administrators -Member "DOMAIN\$newadmin"}
 Invoke-Command -Session $s {Get-LocalGroupMember -Group Administrators | Select-Object -Property Pscomputername,name}
 
 
