75cfe3cad734f06516805fe6f9e313bcbdfb337c
Hunt-j <59659712+Hunt-j@users.noreply.github.com>
2020-02-03 14:08:40 -0500

    Update Cachekill.ps1

--- Cachekill.ps1
+++ Cachekill.ps1
@@ -1,4 +1,18 @@
-﻿#kill processes that need to be closed for caches to clear
+<#
+    .SYNOPSIS
+        Deletes cached files in their most prominent locations
+
+    .DESCRIPTION
+        Removes cached files from firefox, chrome, Edge, and windows temp files, halting associated processes first.
+        
+    .FUNCTIONALITY
+        
+    .NOTES
+            AUTHOR: Hunter Jarvis
+            EMAIL: hjarvis@plymouth.edu
+#>
+
+#kill processes that need to be closed for caches to clear
 get-process -name "*chrome*" | stop-process
 get-process -name "*firefox*" | stop-process
 get-process -name "*adobe*" | Stop-process 
@@ -10,8 +24,10 @@ get-process -name "*onedrive*" | Stop-process
 get-process -name "FileCoAuth" | stop-process
 get-process -name "iexplore" | stop-process
 get-process -name "*edge*" | stop-process
+
 #Sets $user to be whomever the current user is
 $user = $env:username
+
 #Clears recycle bin and every temp file/cache folder that's necessary
 Clear-RecycleBin -Force
 remove-item -path "C:\users\$user\AppData\local\Temp\*" -Recurse -Force 
