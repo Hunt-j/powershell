9d703c21ebc1c4b4da270dec1776c70e7509743e
Hunt-j <59659712+Hunt-j@users.noreply.github.com>
2020-04-28 09:47:22 -0400

    Update Run as Admin as Admin.ps1

--- Run as Admin as Admin.ps1
+++ Run as Admin as Admin.ps1
@@ -1,1 +1,8 @@
-﻿start-process powershell.exe -Credential DOMAIN\USER -NoNewWindow -ArgumentList "Start-Process powershell_ise.exe -verb runas"
+﻿<#This one is for a specific need. You are not logged into a computer as a domain admin,
+   but you need to run powershell as an adminitrator AND a domain admin.
+ #>
+ 
+ $Domain = Read-host "Domain?"
+ $username = Read-host "Username?"
+ 
+ start-process powershell.exe -Credential $domain\$username -NoNewWindow -ArgumentList "Start-Process powershell_ise.exe -verb runas"
